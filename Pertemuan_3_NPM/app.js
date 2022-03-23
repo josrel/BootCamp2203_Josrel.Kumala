b9fcdb86c03791c95e6d966c1bafac75b3308d1b
josrel <cjosrel@gmail.com>
2022-03-23 11:27:26 +0700

    Pertemuan_3_NMP

--- Pertemuan_3_NPM/app.js
+++ Pertemuan_3_NPM/app.js
@@ -1,5 +1,7 @@
 const validator = require('validator')
 const readline = require('readline')
+const { rejects } = require('assert');
+const { resolve } = require('path');
 const fs = require('fs')
 
 const rl = readline.createInterface({
@@ -8,29 +10,31 @@ const rl = readline.createInterface({
 })
 
 // console.log('hello world !')
-console.log(validator.isEmail('josrel@gmail.com'))
+// console.log(validator.isEmail('josrel@gmail.com'))
 
-if(validator.isEmail('cjosrel@gmail.c') == true) {
-    console.log('benar')
-} else{
-    console.log('salah')
-}
-console.log(validator.isMobilePhone('0812345678','id-ID'));
+// if(validator.isEmail('cjosrel@gmail.c') == true) {
+//     console.log('benar')
+// } else{
+//     console.log('salah')
+// }
+// console.log(validator.isMobilePhone('0812345678','id-ID'));
 
+//cara 1
 rl.question('nama anda : ',(nama) =>{
     rl.question('nomor hp : ', (noHP) => {
         rl.question('email : ', (email) => {
-            console.log(`nama anda : ${nama}`)
+            console.log(`nama anda ${nama}`)
             if(validator.isMobilePhone(noHP) == true){
-                console.log(`nomor HP : ${noHP}`)
+                console.log(`nomor HP ${noHP}`)
             }else{
                 console.log('no hp anda salah')
             }
             if(validator.isEmail(email) == true){
-                console.log(`email : ${email}`)
+                console.log(`email ${email}`)
             }else{
-                console.log('email salah ')
+                console.log('email anda salah')
             }
+            rl.close()
         })
     })
 })
