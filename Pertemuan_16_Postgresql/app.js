4b7effc8ac43dfc545672a028fd266b8cf3b6d3a
josrel <cjosrel@gmail.com>
2022-04-11 11:07:27 +0700

    Pertemuan_16_List

---
+++ Pertemuan_16_Postgresql/app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const { del } = require('express/lib/application')
+const app = express()
+const pool = require("./db")
+
+app.use(express.json())
+const port = 3000
+
+app.get("/addasync", async(req,res) => {
+    try{
+        const name = "cobacoba"
+        const mobile = "08122222333"
+        const email = "cobacoba@gmail.com"
+        const newCont = await pool.query(`INSERT INTO contacts values ('${name}','${mobile}','${email}') RETURNING *`)
+        res.json(newCont.rows)
+    } catch(err){
+        console.error(err.message)
+    }
+})
+
+app.get("/list", async(req,res) => {
+    try{
+        const listCont = await pool.query(`SELECT name, mobile, email FROM contacts`)
+        res.json(listCont.rows)
+    } catch(err){
+        console.error(err.message)
+    }
+})
+
+app.get("/updatecontact", async(req,res) => {
+    try{
+        await pool.query(`UPDATE contacts SET email='cobagan@gmail.com' WHERE name='chandra'`)
+        const listCont = await pool.query(`SELECT name, mobile, email FROM contacts`)
+        res.json(listCont.rows)
+    } catch(err){
+        console.error(err.message)
+    }
+})
+
+app.get("/deletecontact", async(req,res) => {
+    try{
+        await pool.query(`DELETE FROM contacts WHERE name='cobacoba'`)
+        const listCont = await pool.query(`SELECT name, mobile, email FROM contacts`)
+        res.json(listCont.rows)
+    } catch(err){
+        console.error(err.message)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
